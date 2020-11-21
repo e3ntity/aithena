77489d32d454eb4e8fd830c6eafeee9bc126fe3a
Lukas Schneider <lu.schneider@protonmail.com>
2020-11-21 04:58:43 -0500

    Added boilerplate for game state

---
+++ src/game/state.cc
@@ -0,0 +1,5 @@
+#include "game/state.h"
+
+namespace aithena {
+
+}
---
+++ src/game/state.h
@@ -0,0 +1,8 @@
+#ifndef AITHENTA_GAME_STATE_H
+#define AITHENTA_GAME_STATE_H
+
+namespace aithena {
+
+}
+
+#endif // AITHENTA_GAME_STATE_H
