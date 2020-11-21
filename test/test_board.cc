13c7eaa92e8fae182cd248426b06e2efa6afa2c4
Lukas Schneider <lu.schneider@protonmail.com>
2020-11-21 15:46:20 -0500

    Added tests for BoardPlane

--- test/test_board.cc
+++ test/test_board.cc
@@ -35,7 +35,7 @@ TEST(BoardPlane, SetsAndClearsCorrectly) {
 }
 
 TEST(BoardPlane, CopiesCorrectly) {
-  aithena::BoardPlane bp(8, 8);
+  aithena::BoardPlane bp(4, 4);
   bp.set(2, 2);
 
   aithena::BoardPlane bp2(bp);
@@ -44,4 +44,83 @@ TEST(BoardPlane, CopiesCorrectly) {
   bp.clear(2, 2);
   ASSERT_FALSE(bp.get(2, 2));
   ASSERT_TRUE(bp2.get(2, 2));
+}
+
+TEST(BoardPlane, AndOperatorWorksCorrectly) {
+  aithena::BoardPlane bp(4, 4);
+  aithena::BoardPlane bp2(4, 4);
+
+  bp.set(1, 3);
+  bp2.set(3, 1);
+
+  aithena::BoardPlane bp3 = bp | bp2;
+
+  for (int y = 0; y < 4; y++) {
+    for (int x = 0; x < 4; x++) {
+      if ((x == 1 && y == 3) || (x == 3 && y == 1))
+        ASSERT_TRUE(bp3.get(x, y));
+      else
+        ASSERT_FALSE(bp3.get(x, y));
+    }
+  }
+}
+
+TEST(BoardPlane, OrOperatorWorksCorrectly) {
+  aithena::BoardPlane bp(4, 4);
+  aithena::BoardPlane bp2(4, 4);
+
+  bp.set(0, 2);
+  bp2.set(0, 2);
+  bp2.set(2, 1);
+
+  aithena::BoardPlane bp3 = bp & bp2;
+
+  for (int y = 0; y < 4; y++) {
+    for (int x = 0; x < 4; x++) {
+      if (x == 0 && y == 2)
+        ASSERT_TRUE(bp3.get(x, y));
+      else
+        ASSERT_FALSE(bp3.get(x, y));
+    }
+  }
+}
+
+TEST(BoardPlane, XorOperatorWorksCorrectly) {
+  aithena::BoardPlane bp(4, 4);
+  aithena::BoardPlane bp2(4, 4);
+
+  bp.set(1, 3);
+  bp2.set(1, 3);
+  bp2.set(3, 1);
+
+  aithena::BoardPlane bp3 = bp ^ bp2;
+
+  for (int y = 0; y < 4; y++) {
+    for (int x = 0; x < 4; x++) {
+      if ((x == 3 && y == 1))
+        ASSERT_TRUE(bp3.get(x, y));
+      else
+        ASSERT_FALSE(bp3.get(x, y));
+    }
+  }
+}
+
+TEST(BoardPlane, EqualityOperatorsWorkCorrectly) {
+  aithena::BoardPlane bp(4, 4);
+  aithena::BoardPlane bp2(4, 4);
+  aithena::BoardPlane bp3(4, 4);
+
+  bp.set(1, 2);
+  bp.set(3, 0);
+  bp2.set(1, 2);
+  bp2.set(3, 0);
+  bp3.set(3, 0);
+
+  ASSERT_TRUE(bp == bp2);
+  ASSERT_FALSE(bp == bp3);
+  ASSERT_FALSE(bp2 == bp3);
+
+  ASSERT_FALSE(bp != bp2);
+  ASSERT_TRUE(bp != bp3);
+  ASSERT_TRUE(bp2 != bp3);
 }
