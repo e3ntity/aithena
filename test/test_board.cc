dac0bb25dc05d52f5f7849e70fc9b4982bd186a6
Lukas Schneider <lu.schneider@protonmail.com>
2020-11-21 15:35:45 -0500

    Added/Adapted more tests for BoardPlane

--- test/test_board.cc
+++ test/test_board.cc
@@ -2,19 +2,46 @@
 
 #include "board/board.h"
 
-TEST(BoardPlane, InitializesCorrectly) {
-	aithena::BoardPlane<8, 8> bp;
+TEST(BoardPlane, InitializesBitfieldToCorrectSize) {
+  aithena::BoardPlane bp(4, 4);
 
-	for (int i = 0; i < 64; i++)
-		ASSERT_EQ(bp.get(i % 8, int(i / 8)), 0);
+  bp.get(3, 3); // Should work
+  ASSERT_DEATH(bp.get(4, 3), ".*pos < m_num_bits.*");
+  ASSERT_DEATH(bp.get(3, 4), ".*pos < m_num_bits.*");
+}
+
+TEST(BoardPlane, InitializesBitfieldToFalse) {
+  aithena::BoardPlane bp(4, 4);
+
+  for (int y = 0; y < 4; y++) {
+    for (int x = 0; x < 4; x++) {
+      ASSERT_FALSE(bp.get(x, y));
+    }
+  }
 }
 
 TEST(BoardPlane, SetsAndClearsCorrectly) {
-	aithena::BoardPlane<8, 8> bp;
+  aithena::BoardPlane bp(4, 4);
 
-	bp.set(4, 2);
-	ASSERT_EQ(bp.get(4, 2), 1);
+  for (int y = 0; y < 4; y++) {
+    for (int x = 0; x < 4; x++) {
+      bp.set(x, y);
+      ASSERT_TRUE(bp.get(x, y));
 
-	bp.clear(4, 2);
-	ASSERT_EQ(bp.get(4, 2), 0);
+      bp.clear(x, y);
+      ASSERT_FALSE(bp.get(x, y));
+    }
+  }
+}
+
+TEST(BoardPlane, CopiesCorrectly) {
+  aithena::BoardPlane bp(8, 8);
+  bp.set(2, 2);
+
+  aithena::BoardPlane bp2(bp);
+
+  ASSERT_TRUE(bp2.get(2, 2));
+  bp.clear(2, 2);
+  ASSERT_FALSE(bp.get(2, 2));
+  ASSERT_TRUE(bp2.get(2, 2));
 }
