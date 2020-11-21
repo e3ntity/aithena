6d24a45bb54ffcf4e5e0ea92aecd97c09ce3c3f4
PeanutI3utter <sijunkwon@gmail.com>
2020-11-21 12:13:30 +0100

    Action abstract class implemented

--- src/game/action.h
+++ src/game/action.h
@@ -1,7 +1,20 @@
 #ifndef AITHENTA_GAME_ACTION
 #define AITHENTA_GAME_ACTION
 
+#include "board/state.h"
+#include <string>
+#include <tuple>
+
 namespace aithena {
+
+class Action
+{
+public:
+	Action(State before, State after) : before_after(std::make_tuple(before, after)) {}
+	virtual std::string to_string();
+private:
+	std::tuple<State, State> before_after;
+};
 
 }
 
