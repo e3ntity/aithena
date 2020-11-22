6fb959d0ab57c51b9127142bf3b17360ff2fa9e4
PeanutI3utter <sijunkwon@gmail.com>
2020-11-22 13:31:43 +0100

    Fixed width and height assertion

--- src/chess/game.cc
+++ src/chess/game.cc
@@ -1,21 +1,19 @@
-#include "chess/chess.h"
-
 #include <assert.h>
+
+#include "chess/chess.h"
 
 namespace aithena {
 namespace chess {
 
 Game::Game() : Game{Options{}} {}
 
-Game::Game(Options options) : ::aithena::Game<Action, State> {options} {
+Game::Game(Options options) : ::aithena::Game<Action, State>{options} {
   DefaultOption("board_width", 8);
   DefaultOption("board_height", 8);
   DefaultOption("figure_count", static_cast<unsigned char>(Figure::kCount));
 
-  assert((
-    "Chess board can at most be 8x8",
-    GetOption("board_width") < 8 && GetOption("board_height") < 8
-  ));
+  assert(("Chess board can at most be 8x8",
+          GetOption("board_width") <= 8 && GetOption("board_height") <= 8));
 }
 
 State Game::GetInitialState() {
@@ -27,5 +25,5 @@ Game::ActionList Game::GetLegalActions(State state) {
   return std::vector<Action>();
 };
 
-}
-}
+}  // namespace chess
+}  // namespace aithena
