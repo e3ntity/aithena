b43cef45d91e94a0d1352d6e0e3c0672c362f0fb
Lukas Schneider <lu.schneider@protonmail.com>
2020-11-22 08:08:33 -0500

    Updated formatting

--- src/chess/game.cc
+++ src/chess/game.cc
@@ -7,13 +7,15 @@ namespace chess {
 
 Game::Game() : Game{Options{}} {}
 
-Game::Game(Options options) : ::aithena::Game<Action, State>{options} {
+Game::Game(Options options) : ::aithena::Game<Action, State> {options} {
   DefaultOption("board_width", 8);
   DefaultOption("board_height", 8);
   DefaultOption("figure_count", static_cast<unsigned char>(Figure::kCount));
 
-  assert(("Chess board can at most be 8x8",
-          GetOption("board_width") <= 8 && GetOption("board_height") <= 8));
+  assert((
+    "Chess board can at most be 8x8",
+    GetOption("board_width") <= 8 && GetOption("board_height") <= 8
+  ));
 }
 
 State Game::GetInitialState() {
