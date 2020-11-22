29bbc8082bed2dd0cfbdec096c2c9f8c71ebc518
Lukas Schneider <lu.schneider@protonmail.com>
2020-11-22 12:12:23 -0500

    Added new chess state member function implementations

--- src/chess/state.cc
+++ src/chess/state.cc
@@ -3,11 +3,38 @@
 namespace aithena {
 namespace chess {
 
-State::State(std::size_t width, std::size_t height, unsigned char figure_count)
-  : ::aithena::State(width, height, figure_count) {}
-State::State(Board board) : ::aithena::State(board) {}
+State::State(
+  std::size_t width,
+  std::size_t height,
+  unsigned figure_count = static_cast<unsigned>(Figure::kCount))
+  : ::aithena::State(width, height, figure_count),
+    player_{Player::kWhite},
+    castle_queen_{false, false},
+    castle_king_{false, false},
+    move_count_{0},
+    no_progress_count_{0} {}
 
-State::State(const State& other) : ::aithena::State(other) {}
+State::State(const State& other)
+  : ::aithena::State(other),
+    player_{other.player_},
+    castle_queen_{other.castle_queen_},
+    castle_king_{other.castle_king_},
+    move_count_{other.move_count_},
+    no_progress_count_{other.no_progress_count_} {}
+
+State& State::operator=(const State& other) {
+  if (this == &other) return *this;
+
+  ::aithena::State::operator=(other);
+
+  player_ = other.player_;
+  castle_queen_ = other.castle_queen_;
+  castle_king_ = other.castle_king_;
+  move_count_ = other.move_count_;
+  no_progress_count_ = other.no_progress_count_;
+
+  return *this;
+}
 
 const std::string State::ToString() const {
   return "";
