0c43fbb2611ea32a1abca9106a34b73dbd4f29c2
Lukas Schneider <lu.schneider@protonmail.com>
2020-11-23 04:21:40 -0500

    Added a default board for games GetInitialState method

--- src/chess/game.cc
+++ src/chess/game.cc
@@ -22,6 +22,25 @@ State Game::GetInitialState() {
   State state(GetOption("board_width"), GetOption("board_height"),
               GetOption("figure_count"));
 
+  assert(GetOption("board_width") == 8 && GetOption("board_height") == 8);
+
+  Board& board = state.GetBoard();
+  auto white_pawn = aithena::chess::make_piece(
+                      aithena::chess::Figure::kPawn,
+                      aithena::chess::Player::kWhite);
+  auto black_pawn = aithena::chess::make_piece(
+                      aithena::chess::Figure::kPawn,
+                      aithena::chess::Player::kBlack);
+
+  board.SetField(1, 1, white_pawn);
+  board.SetField(2, 1, white_pawn);
+  board.SetField(5, 1, white_pawn);
+  board.SetField(6, 1, white_pawn);
+  board.SetField(1, 6, black_pawn);
+  board.SetField(2, 6, black_pawn);
+  board.SetField(5, 6, black_pawn);
+  board.SetField(6, 6, black_pawn);
+
   return state;
 }
 
