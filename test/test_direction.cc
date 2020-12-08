3417712abe9769f3d6783f840d33707442c660f4
PeanutI3utter <sijunkwon@gmail.com>
2020-12-08 16:28:05 +0100

    Test for Direction fixed

--- test/test_direction.cc
+++ test/test_direction.cc
@@ -1,30 +1,34 @@
 #include "gtest/gtest.h"
 
-#include "chess/direction.h"
+#include "chess/moves.h"
 
 TEST(Direction, DirectionInitializesCorrectly) {
-	aithena::Direction dir{2, -4};
-	ASSERT_EQ(dir.getX(), 2);
-	ASSERT_EQ(dir.getY(), -4);
-	aithena::Direction dir2;
-	ASSERT_EQ(dir2.getX(), 0);
-	ASSERT_EQ(dir2.getY(), 0);
+	aithena::chess::Direction dir{2, -4};
+	ASSERT_EQ(dir.x, 2);
+	ASSERT_EQ(dir.y, -4);
+	aithena::chess::Direction dir2{0, 0};
+	ASSERT_EQ(dir2.x, 0);
+	ASSERT_EQ(dir2.y, 0);
 }
 
 TEST(Direction, DirectionAddsCorrectly) {
-	aithena::Direction upleft = aithena::up + aithena::left;
-	ASSERT_EQ(upleft.getX(), -1);
-	ASSERT_EQ(upleft.getY(), 1);
-	aithena::Direction upright = aithena::up + aithena::right;
-	ASSERT_EQ(upright.getX(), 1);
-	ASSERT_EQ(upright.getY(), 1);
-	aithena::Direction downleft = aithena::down + aithena::left;
-	ASSERT_EQ(downleft.getX(), -1);
-	ASSERT_EQ(downleft.getY(), -1);
-	aithena::Direction downright = aithena::down + aithena::right;
-	ASSERT_EQ(downright.getX(), 1);
-	ASSERT_EQ(downright.getY(), -1);
-	aithena::Direction none = aithena::up + aithena::down;
-	ASSERT_EQ(none.getX(), 0);
-	ASSERT_EQ(none.getY(), 0);
+	aithena::chess::Direction upleft = aithena::chess::up
+	                                   + aithena::chess::left;
+	ASSERT_EQ(upleft.x, -1);
+	ASSERT_EQ(upleft.y, 1);
+	aithena::chess::Direction upright = aithena::chess::up
+	                                    + aithena::chess::right;
+	ASSERT_EQ(upright.x, 1);
+	ASSERT_EQ(upright.y, 1);
+	aithena::chess::Direction downleft = aithena::chess::down
+	                                     + aithena::chess::left;
+	ASSERT_EQ(downleft.x, -1);
+	ASSERT_EQ(downleft.y, -1);
+	aithena::chess::Direction downright = aithena::chess::down
+	                                      + aithena::chess::right;
+	ASSERT_EQ(downright.x, 1);
+	ASSERT_EQ(downright.y, -1);
+	aithena::chess::Direction none = aithena::chess::up + aithena::chess::down;
+	ASSERT_EQ(none.x, 0);
+	ASSERT_EQ(none.y, 0);
 }
