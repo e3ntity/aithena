ad3383c2e94c2a7e4f514824b865421d757e5a13
Lukas Schneider <schneider.lukas@protonmail.com>
2021-06-06 09:58:23 +0200

    reformatted file

--- aithena/benchmark.cc
+++ aithena/benchmark.cc
@@ -1,8 +1,8 @@
-#include "main.h"
-
 #include <getopt.h>
+
 #include <iostream>
 
+#include "./main.h"
 #include "chess/game.h"
 #include "chess/util.h"
 
@@ -23,13 +23,12 @@ std::string GetBenchmarkUsageText() {
 }
 
 int RunBenchmark(int argc, char** argv) {
-  static struct option long_options[] = {
-      {"help", no_argument, nullptr, 'h'},
-      {"depth", required_argument, nullptr, 'd'},
-      {"fen", required_argument, nullptr, 'f'},
-      {"max-moves", required_argument, nullptr, 'm'},
-      {"max-no-progress", required_argument, nullptr, 'p'},
-      {0, 0, 0, 0}};
+  static struct option long_options[] = {{"help", no_argument, nullptr, 'h'},
+                                         {"depth", required_argument, nullptr, 'd'},
+                                         {"fen", required_argument, nullptr, 'f'},
+                                         {"max-moves", required_argument, nullptr, 'm'},
+                                         {"max-no-progress", required_argument, nullptr, 'p'},
+                                         {0, 0, 0, 0}};
 
   // Configurable options
   std::string fen{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"};
@@ -67,11 +66,10 @@ int RunBenchmark(int argc, char** argv) {
   }
 
   chess::State::StatePtr start = chess::State::FromFEN(fen);
-  chess::Game::Options options = {
-      {"board_width", start->GetBoard().GetWidth()},
-      {"board_height", start->GetBoard().GetHeight()},
-      {"max_no_progress", max_no_progress},
-      {"max_move_count", max_moves}};
+  chess::Game::Options options = {{"board_width", start->GetBoard().GetWidth()},
+                                  {"board_height", start->GetBoard().GetHeight()},
+                                  {"max_no_progress", max_no_progress},
+                                  {"max_move_count", max_moves}};
   chess::Game::GamePtr game = std::make_shared<chess::Game>(options);
 
   Benchmark bm_perft;
@@ -85,17 +83,13 @@ int RunBenchmark(int argc, char** argv) {
 
   bm_perft.End();
 
-  double nps = 1000000.0 * static_cast<double>(nodes) /
-               static_cast<double>(bm_perft.GetLast(Benchmark::UNIT_USEC));
+  double nps = 1000000.0 * static_cast<double>(nodes) / static_cast<double>(bm_perft.GetLast(Benchmark::UNIT_USEC));
 
   bm_bm.End();
 
-  std::cout << "## Benchmark (completed in "
-            << bm_bm.GetLast(Benchmark::UNIT_SEC) << " seconds) ##"
-            << std::endl;
-  std::cout << "Perft(" << perft_depth << "): Searched " << nodes
-            << " nodes in " << bm_perft.GetLast(Benchmark::UNIT_SEC)
-            << " seconds (" << nps << " nps)" << std::endl;
+  std::cout << "## Benchmark (completed in " << bm_bm.GetLast(Benchmark::UNIT_SEC) << " seconds) ##" << std::endl;
+  std::cout << "Perft(" << perft_depth << "): Searched " << nodes << " nodes in "
+            << bm_perft.GetLast(Benchmark::UNIT_SEC) << " seconds (" << nps << " nps)" << std::endl;
 
   return 0;
 }
