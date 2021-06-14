14833a487aba41e96c41aac0c47c9d4fe796d6ba
Lukas Schneider <schneider.lukas@protonmail.com>
2021-06-14 10:16:24 +0200

    updated main executable

--- aithena/game.cc
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-
-#include <iostream>
-
-int RunGame(int argc, char** argv) {
-  std::cout << "Running Game" << std::endl;
-
-  return 0;
-}
--- aithena/main.cc
+++ aithena/main.cc
@@ -8,17 +8,19 @@
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <iostream>
 #include <string>
+
+#include "alphazero.h"
+#include "benchmark.h"
 
 std::string GetUsageText() {
   return "Usage: ./aithena <program> <options>\n"
          "## Programs ##\n"
+         "  alphazero   Run AlphaZero\n"
          "  benchmark   Benchmark the move generation\n"
          "  version     Print version information\n"
-         //"  game        Run an interactive game with its own prompt\n"
-         "  mcts        Run MCTS\n"
-         //"  alphazero   Run AlphaZero\n"
          "(Run programs with \"-h\" to list available options)\n";
 }
 
@@ -44,8 +46,6 @@ int main(int argc, char** argv) {
   }
 
   if (program == "benchmark") return RunBenchmark(args_len, args);
-  // if (program == "game") return RunGame(args_len, args);
-  if (program == "mcts") return RunMCTS(args_len, args);
   if (program == "alphazero") return RunAlphazero(args_len, args);
 
   std::cout << "[!] Invalid program \"" << program << "\"" << std::endl;
