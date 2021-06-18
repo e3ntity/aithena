0c99ab46bd50ab4539f3821ecb89f9a9765c182b
Lukas Schneider <schneider.lukas@protonmail.com>
2021-06-18 10:38:10 +0200

    formatted perft tests

--- test/test_perft.cc
+++ test/test_perft.cc
@@ -2,10 +2,9 @@
 
 #include <iostream>
 
-#include "gtest/gtest.h"
-
 #include "chess/game.h"
 #include "chess/util.h"
+#include "gtest/gtest.h"
 
 using namespace aithena;
 
@@ -20,8 +19,7 @@ class ChessPerftTest : public ::testing::Test {
 };
 
 TEST_F(ChessPerftTest, InitialPosition) {
-  auto state = chess::State::FromFEN(
-      "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+  auto state = chess::State::FromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
 
   EXPECT_EQ(perft(game_, state, 5), 5072212);
 }
@@ -66,29 +64,25 @@ TEST_F(ChessPerftTest, Custom2) {
 }
 
 TEST_F(ChessPerftTest, Custom3) {
-  auto state = chess::State::FromFEN(
-      "r1bqkbnr/pppppppp/n7/8/8/P7/1PPPPPPP/RNBQKBNR w KQkq - 2 2");
+  auto state = chess::State::FromFEN("r1bqkbnr/pppppppp/n7/8/8/P7/1PPPPPPP/RNBQKBNR w KQkq - 2 2");
 
   EXPECT_EQ(perft(game_, state, 1), 19);
 }
 
 TEST_F(ChessPerftTest, Custom4) {
-  auto state = chess::State::FromFEN(
-      "r3k2r/p1pp1pb1/bn2Qnp1/2qPN3/1p2P3/2N5/PPPBBPPP/R3K2R b KQkq - 3 2");
+  auto state = chess::State::FromFEN("r3k2r/p1pp1pb1/bn2Qnp1/2qPN3/1p2P3/2N5/PPPBBPPP/R3K2R b KQkq - 3 2");
 
   EXPECT_EQ(perft(game_, state, 1), 5);
 }
 
 TEST_F(ChessPerftTest, Custom5) {
-  auto state = chess::State::FromFEN(
-      "2kr3r/p1ppqpb1/bn2Qnp1/3PN3/1p2P3/2N5/PPPBBPPP/R3K2R b KQ - 3 2");
+  auto state = chess::State::FromFEN("2kr3r/p1ppqpb1/bn2Qnp1/3PN3/1p2P3/2N5/PPPBBPPP/R3K2R b KQ - 3 2");
 
   EXPECT_EQ(perft(game_, state, 1), 44);
 }
 
 TEST_F(ChessPerftTest, Custom6) {
-  auto state = chess::State::FromFEN(
-      "rnb2k1r/pp1Pbppp/2p5/q7/2B5/8/PPPQNnPP/RNB1K2R w KQ - 3 9");
+  auto state = chess::State::FromFEN("rnb2k1r/pp1Pbppp/2p5/q7/2B5/8/PPPQNnPP/RNB1K2R w KQ - 3 9");
 
   EXPECT_EQ(perft(game_, state, 1), 39);
 }
@@ -100,16 +94,13 @@ TEST_F(ChessPerftTest, Custom7) {
 }
 
 TEST_F(ChessPerftTest, Custom8) {
-  auto state = chess::State::FromFEN(
-      "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8");
+  auto state = chess::State::FromFEN("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8");
 
   EXPECT_EQ(perft(game_, state, 4), 2167396);
 }
 
 TEST_F(ChessPerftTest, Custom9) {
-  auto state = chess::State::FromFEN(
-      "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 "
-      "10");
+  auto state = chess::State::FromFEN("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10");
 
   EXPECT_EQ(perft(game_, state, 3), 92015);
 }
@@ -135,36 +126,31 @@ TEST_F(ChessPerftTest, Custom12) {
 // Test positions from https://www.chessprogramming.org/Perft_Results
 
 TEST_F(ChessPerftTest, Kiwipete) {
-  auto state = chess::State::FromFEN(
-      "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
+  auto state = chess::State::FromFEN("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
 
   EXPECT_EQ(perft(game_, state, 4), 4185552);
 }
 
 TEST_F(ChessPerftTest, Custom13) {
-  auto state = chess::State::FromFEN(
-      "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1");
+  auto state = chess::State::FromFEN("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1");
 
   EXPECT_EQ(perft(game_, state, 5), 720879);
 }
 
 TEST_F(ChessPerftTest, Custom14) {
-  auto state = chess::State::FromFEN(
-      "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");
+  auto state = chess::State::FromFEN("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");
 
   EXPECT_EQ(perft(game_, state, 4), 432070);
 }
 
 TEST_F(ChessPerftTest, Custom15) {
-  auto state = chess::State::FromFEN(
-      "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1");
+  auto state = chess::State::FromFEN("r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1");
 
   EXPECT_EQ(perft(game_, state, 4), 432070);
 }
 
 TEST_F(ChessPerftTest, Custom16) {
-  auto state = chess::State::FromFEN(
-      "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10");
+  auto state = chess::State::FromFEN("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10");
 
   EXPECT_EQ(perft(game_, state, 4), 3986609);
 }
