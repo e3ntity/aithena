dcd30b6e82337c88ebed23462c90a477e2bae810
Lukas Schneider <schneider.lukas@protonmail.com>
2021-07-09 10:07:45 +0200

    added timestamped saving option and configurability of power-uct p-value

--- aithena/alphazero.h
+++ aithena/alphazero.h
@@ -1,6 +1,12 @@
 #ifndef AITHENA_ALPHAZERO_H_
 #define AITHENA_ALPHAZERO_H_
 
+#include <string>
+
+static std::string aithena_start_timestamp;
+
+std::string GetTimestamp();
+
 int RunAlphazero(int argc, char** argv);
 
 #endif  // AITHENA_ALPHAZERO_H_
